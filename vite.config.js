6ee201ff9a17014ee47985ce94474046d23042fe
Akilesh A <akileshanand302006@gmail.com>
2026-08-24 21:52:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Akilesh-A-Software-Systems-Portfolio/',
   build: {
     rollupOptions: {
       output: {
